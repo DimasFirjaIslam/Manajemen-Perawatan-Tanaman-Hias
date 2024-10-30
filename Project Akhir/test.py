a2b5ddfacf9ac74e3b735628d5bc5a0955ca2a29
Trigusni <109473523+MTHermawan@users.noreply.github.com>
2024-10-30 10:41:03 +0800

    Contributors Push Test

---
+++ Project Akhir/test.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("clear || cls")
+
+users = [
+    {"Username": "Dimas", "Password": "057", "Role": "Admin"},
+    {"Username": "User", "Password": "000", "Role": "User"}
+]
+
+data_tanaman = [
+    {"Nama": "Bunga Kamboja", "Jenis": "Bonsai Bunga", "Jadwal_siram": "3 Hari Sekali"},
+    {"Nama": "Sirih Gading", "Jenis": "Tropis Merambat", "Jadwal_siram": "4 Hari Sekali"},
+    {"Nama": "Lidah Buaya", "Jenis": "Sukulen", "Jadwal_siram": "Seminggu Sekali"}
+]
+
+print("Hello World")
